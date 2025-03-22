4b16f7e4b74a5020ae27ef611f9b8dc6c1b0c56a
rwq3 <rwq3@cornell.edu>
2025-03-22 01:17:30 -0400

    fix: remove debug print in pigeon.ml

--- game/pigeon.ml
+++ game/pigeon.ml
@@ -4,7 +4,6 @@ open Transformations
 
 let pigeon_action_generator (state : GameState.t) (entity : GameEntity.t) _ :
     GameState.transition option =
-  print_endline "Pigeon Action!";
   let random_direction : vec2 = (1 - Random.int 3, 1 - Random.int 3) in
   let target_pos = add_vec2 random_direction entity.pos in
   if GameWorld.mem_pos state.world target_pos then
