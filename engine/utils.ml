297aa48456bf2d04dcab31e3f9a3e8271021902b
Paul Fischer <ptf34@cornell.edu>
2025-03-28 05:50:34 -0400

    feat: adding vec utility
    - Added additional utility methods for vec2's for use on matrices

--- engine/utils.ml
+++ engine/utils.ml
@@ -1,8 +1,35 @@
 type vec2 = int * int
 
+(*Translation methods*)
 let add_vec2 (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 let neg_vec2 (x1, y1) = (-1 * x1, -1 * y1)
 let sub_vec2 vec1 vec2 = add_vec2 vec1 (neg_vec2 vec2)
+
+(*2d array access*)
+type 'a matrix = 'a array array
+
+let get_at_vec (arr : 'a matrix) (row, col) = arr.(row).(col)
+
+let get_at_vec_opt (arr : 'a matrix) (row, col) =
+  try Some arr.(row).(col) with Invalid_argument _ -> None
+
+let set_at_vec (arr : 'a matrix) (row, col) x = arr.(row).(col) <- x
+
+(*Misc*)
+let neighbors vec =
+  [
+    add_vec2 vec (0, 1);
+    add_vec2 vec (1, 0);
+    add_vec2 vec (0, -1);
+    add_vec2 vec (-1, 0);
+  ]
+
+let random_dir () =
+  match Random.int 4 with
+  | 0 -> (1, 0)
+  | 1 -> (0, 1)
+  | 2 -> (-1, 0)
+  | _ -> (0, -1)
 
 let string_of_vec vec =
   "(" ^ string_of_int (fst vec) ^ "," ^ string_of_int (snd vec) ^ ")"
--- engine/utils.mli
+++ engine/utils.mli
@@ -2,13 +2,33 @@ type vec2 = int * int
 (**[vec2] describes the location of an entity in 2D cartesian space*)
 
 val add_vec2 : vec2 -> vec2 -> vec2
-(**[add_vec2 vec1 vec2] returns the sum of two positions*)
+(**[add_vec2 vec1 vec2] is the sum of two positions*)
 
 val neg_vec2 : vec2 -> vec2
-(**[neg_vec2 vec] returns the negated version of [vec]*)
+(**[neg_vec2 vec] is the negated version of [vec]*)
 
 val sub_vec2 : vec2 -> vec2 -> vec2
-(**[sub_vec2 vec1] returns [(fst vec1 - fst vec2, snd vec1 - snd vec2)]*)
+(**[sub_vec2 vec1] is [(fst vec1 - fst vec2, snd vec1 - snd vec2)]*)
+
+val get_at_vec : 'a array array -> vec2 -> 'a
+(**[get_at_vec matrix vec] is the element in [matrix] at location [vec]. Raises
+   [Invalid_argument "index out of bounds"] if [vec2] does not represent a valid
+   location in [matrix]. *)
+
+val get_at_vec_opt : 'a array array -> vec2 -> 'a option
+(**[get_at_vec_opt matrix vec] is an optional element in [matrix] at location
+   [vec].*)
+
+val set_at_vec : 'a array array -> vec2 -> 'a -> unit
+(**[set_at_vec matrix vec x] sets the location [vec] in [matrix] to [x]. Raises
+   [Invalid_argument "index out of bounds"] if [vec2] does not represent a valid
+   location in [matrix]. *)
+
+val neighbors : vec2 -> vec2 list
+(**[neighbors vec] is the list of the 4 vec2s adjacent to [vec] *)
+
+val random_dir : unit -> vec2
+(**[random_dir] is a random vec2 offset. *)
 
 val string_of_vec : vec2 -> string
-(**[string_of_vec vec] converts [vec] into a string form*)
+(**[string_of_vec vec] is a string representation of [vec]*)
