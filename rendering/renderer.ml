cacb2b3d19174c130da66694368f1934e591cd44
rwq3 <rwq3@cornell.edu>
2025-04-01 21:28:34 -0500

    feat: letterboxed rendering

--- rendering/renderer.ml
+++ rendering/renderer.ml
@@ -17,54 +17,73 @@ end
 
 module RenderableSet = Set.Make (ComparableRenderable)
 
-type t = RenderableSet.t
+type t = {
+  renderables : RenderableSet.t;
+  source_state : GameState.t;
+}
+
 type input_handler = GameState.t -> GameState.input -> GameState.t
 
 let update_render_state (renderer : t) (game_state : GameState.t) =
-  GameWorld.all_entities game_state.world
-  |> List.fold_left
-       (fun (updated_renderer : RenderableSet.t) (entity : GameEntity.t) ->
-         let renderer_opt =
-           RenderableSet.find_first_opt
-             (fun (current_renderable : renderable) ->
-               current_renderable.source_entity.id >= entity.id)
-             updated_renderer
-         in
-         match renderer_opt with
-         | None ->
-             RenderableSet.add
-               { source_entity = entity; rendered_pos = (0., 0.) }
-               updated_renderer
-         | Some exising_renderable ->
-             let temp_renderer =
-               RenderableSet.remove exising_renderable updated_renderer
+  {
+    renderables =
+      GameWorld.all_entities game_state.world
+      |> List.fold_left
+           (fun (updated_renderer : RenderableSet.t) (entity : GameEntity.t) ->
+             let renderer_opt =
+               RenderableSet.find_first_opt
+                 (fun (current_renderable : renderable) ->
+                   current_renderable.source_entity.id >= entity.id)
+                 updated_renderer
              in
-             RenderableSet.add
-               {
-                 source_entity = entity;
-                 rendered_pos = exising_renderable.rendered_pos;
-               }
-               temp_renderer)
-       renderer
+             match renderer_opt with
+             | None ->
+                 RenderableSet.add
+                   { source_entity = entity; rendered_pos = (0., 0.) }
+                   updated_renderer
+             | Some existing_renderable ->
+                 let temp_renderer =
+                   RenderableSet.remove existing_renderable updated_renderer
+                 in
+                 RenderableSet.add
+                   {
+                     source_entity = entity;
+                     rendered_pos = existing_renderable.rendered_pos;
+                   }
+                   temp_renderer)
+           renderer.renderables;
+    source_state = renderer.source_state;
+  }
 
 let font_scaling_factor = 32.0
 
 let tick (renderer : t) =
-  RenderableSet.map
-    (fun current_renderable ->
-      {
-        source_entity = current_renderable.source_entity;
-        rendered_pos =
-          lerp_vec current_renderable.rendered_pos
-            (vec2f_of_vec2 current_renderable.source_entity.pos)
-            0.5;
-      })
-    renderer
+  {
+    renderables =
+      RenderableSet.map
+        (fun current_renderable ->
+          {
+            source_entity = current_renderable.source_entity;
+            rendered_pos =
+              lerp_vec current_renderable.rendered_pos
+                (vec2f_of_vec2 current_renderable.source_entity.pos)
+                0.5;
+          })
+        renderer.renderables;
+    source_state = renderer.source_state;
+  }
+
+let draw_frame w =
+  Raylib.draw_rectangle_lines_ex
+    (Raylib.Rectangle.create 0. 0.
+       (float_of_int (Raylib.get_screen_width ()))
+       (float_of_int (Raylib.get_screen_height ())))
+    (w *. 2.) Color.black
 
 let render (renderer : t) =
   Raylib.begin_drawing ();
   Raylib.clear_background Color.white;
-  RenderableSet.to_list renderer
+  RenderableSet.to_list renderer.renderables
   |> List.iter (fun (r : renderable) ->
          let screen_space_position =
            ( float_of_int (Raylib.get_screen_width () / 2),
@@ -97,6 +116,7 @@ let render (renderer : t) =
                (snd screen_space_position)
                (int_of_float font_scaling_factor)
                Color.black);
+  draw_frame 80.;
   Raylib.end_drawing ()
 
 let rec loop_aux (renderer : t) (game_state : GameState.t)
@@ -137,7 +157,11 @@ let loop (renderer : t) (game_state : GameState.t)
   loop_aux renderer game_state input_handler
 
 let make_from_state (game_state : GameState.t) =
-  GameWorld.all_entities game_state.world
-  |> List.map (fun (entity : GameEntity.t) ->
-         { source_entity = entity; rendered_pos = vec2f_of_vec2 entity.pos })
-  |> RenderableSet.of_list
+  {
+    renderables =
+      GameWorld.all_entities game_state.world
+      |> List.map (fun (entity : GameEntity.t) ->
+             { source_entity = entity; rendered_pos = vec2f_of_vec2 entity.pos })
+      |> RenderableSet.of_list;
+    source_state = game_state;
+  }
