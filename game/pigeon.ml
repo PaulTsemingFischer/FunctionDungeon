35bd82479b6145c316761beca7224221413cc4dc
rwq3 <rwq3@cornell.edu>
2025-04-05 00:39:42 -0500

    feat: add action and move modifier activation

--- game/pigeon.ml
+++ game/pigeon.ml
@@ -2,8 +2,11 @@ open Root
 open Engine.Utils
 
 let pigeon_action (state : GameState.t) (entity : GameEntity.t) _ =
-  let random_direction = random_element entity.stats.base_moves in
+  let updated_state, possible_moves =
+    GameState.activate_move_modifiers state entity entity.stats.base_moves
+  in
+  let random_direction = random_element possible_moves in
   let target_position = add_vec2 random_direction entity.pos in
-  if GameWorld.mem_pos (GameState.get_world state) target_position then
-    Transformations.say state entity "<pigeon noises>"
-  else Transformations.apply_move state entity random_direction
+  if GameWorld.mem_pos (GameState.get_world updated_state) target_position then
+    Transformations.say updated_state entity "<pigeon noises>"
+  else Transformations.apply_move updated_state entity random_direction
