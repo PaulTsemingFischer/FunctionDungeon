6ee55cddfff133afe6946fd07131ff5b1d96a703
mae97 <133041959+michaelaeichel@users.noreply.github.com>
2025-04-08 12:09:43 -0400

    enemie and obstacle type defs

---
+++ game/enemies.ml
@@ -0,0 +1,18 @@
+open Root
+open Engine.Utils
+
+type enemy =
+  | Jailer of int * int
+    (* fence player in r radius from their current position for t turns *)
+  | Thief (* Randomly take one of player's items *)
+  | Blinder of
+      int (* player can't see any of the board/must go by memory for t turns *)
+  | Fog_Cloud of int * int
+(* player can't see more than r radius around current position for t turns *)
+
+let string_of_enemy (e : enemy) =
+  match e with
+  | Jailer (r, t) -> "jailer"
+  | Thief -> "thief"
+  | Blinder t -> "blinder"
+  | Fog_Cloud (r, t) -> "fog cloud"
