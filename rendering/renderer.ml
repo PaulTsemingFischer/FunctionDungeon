49f6452359f54427c08be46ba365c425f43d8130
mae97 <michaelaeichel@gmail.com>
2025-04-26 14:23:06 -0400

    add enemy and obstacle entities to rendering

--- rendering/renderer.ml
+++ rendering/renderer.ml
@@ -286,37 +286,46 @@ let render (renderer : t) =
                (snd screen_space_position)
                (int_of_float tile_scaling_factor)
                Color.black
-         | Water ->
-             Raylib.draw_text "~"
-               (fst screen_space_position)
-               (snd screen_space_position)
-               (int_of_float tile_scaling_factor)
-               Color.blue
-         | Lava ->
-             Raylib.draw_text "~"
-               (fst screen_space_position)
-               (snd screen_space_position)
-               (int_of_float tile_scaling_factor)
-               Color.orange
-         | Fire ->
-             Raylib.draw_text "♨"
-               (fst screen_space_position)
-               (snd screen_space_position)
-               (int_of_float tile_scaling_factor)
-               Color.red
-         | Rock ->
-             Raylib.draw_text "o"
-               (fst screen_space_position)
-               (snd screen_space_position)
-               (int_of_float tile_scaling_factor)
-               Color.gray
-         | HorizontalBouncer is_moving_right ->
-             Raylib.draw_text
-               (if is_moving_right then ">" else "<")
-               (fst screen_space_position)
-               (snd screen_space_position)
-               (int_of_float tile_scaling_factor)
-               Color.black);
+         | Enemy e -> (
+             match e with
+             | Jailer (r, t) ->
+                 Raylib.draw_text "j"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black
+             | Thief ->
+                 Raylib.draw_text "t"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black
+             | Blinder t ->
+                 Raylib.draw_text "b"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black
+             | Fog_Cloud (r, t) ->
+                 Raylib.draw_text "c"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black)
+         | Obstacle o -> (
+             match o with
+             | Spreading_Fire (c, r, g) ->
+                 Raylib.draw_text "f"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black
+             | Fence ->
+                 Raylib.draw_text "fn"
+                   (fst screen_space_position)
+                   (snd screen_space_position)
+                   (int_of_float tile_scaling_factor)
+                   Color.black));
 
   end_mode_2d ();
   draw_ui renderer;
