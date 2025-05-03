c1f93f3e475eee86cfd1ac81b8e9bd53a8453642
rwq3 <rwq3@cornell.edu>
2025-05-03 13:56:50 -0400

    task: fix printing latest events

--- game/GameState.ml
+++ game/GameState.ml
@@ -90,11 +90,13 @@ let print_events state =
       print_endline (string_of_int turn ^ "\t" ^ string_of_event event))
     (List.rev state.events)
 
-let print_latest_event state =
-  if List.length state.events > 0 then
-    let turn, event = List.hd state.events in
-    print_endline (string_of_int turn ^ "\t" ^ string_of_event event)
-  else print_endline "No events"
+let print_latest_events (state : t) =
+  List.iter
+    (fun ((turn, event) : int * event) : unit ->
+      if turn = state.turn - 1 then
+        print_endline (string_of_int turn ^ "\t" ^ string_of_event event)
+      else ())
+    state.events
 
 let query_update_player state =
   let updated_player =
@@ -128,7 +130,7 @@ let step (state : t) (input : input) =
           (GameWorld.all_entities state_ext.world))
       state state.transitions
   in
-  print_latest_event state;
+  print_latest_events state;
   let updated_state =
     {
       world = new_state.world;
