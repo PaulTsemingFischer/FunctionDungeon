fcdd4907fcd2a7e717fd356e9909687ec1ab8b88
mae97 <michaelaeichel@gmail.com>
2025-05-03 13:15:28 -0400

    set up test files for enemies and obstacles

--- game/obstacles.ml
+++ game/obstacles.ml
@@ -32,9 +32,9 @@ let get_g obstacle =
 let update_obstacle_age obstacle =
   match obstacle with
   | Fence t -> Fence (t - 1)
-  | Spreading_Fire (c, r, g) -> raise WrongObsType
+  | Spreading_Fire (c, r, g) -> Spreading_Fire (c, r, g)
 
 let grow_fire obstacle =
   match obstacle with
   | Fence t -> raise WrongObsType
-  | Spreading_Fire (c, r, g) -> Spreading_Fire (c, r + g, g)
+  | Spreading_Fire (c, r, g) -> Spreading_Fire (c, r + 1, g)
