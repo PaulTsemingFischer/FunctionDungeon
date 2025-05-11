a7dd1f73033678310156f39a34b3ed6b70f3e753
Paul Fischer <ptf34@cornell.edu>
2025-05-11 02:25:01 -0400

    fix: randomness
    - Added random init to main

--- bin/main.ml
+++ bin/main.ml
@@ -20,8 +20,15 @@ let setup () =
   Raylib.init_window 1000 1000 "raylib [core] example - basic window";
   Raylib.set_target_fps 60
 
+(* let () = Printexc.record_backtrace true *)
+
 let () =
-  (* setup (); *)
-  let state = generate_starting_state () in
-  let renderer = Renderer.make_from_state state in
-  Renderer.loop renderer state GameState.step
+  try
+    Random.self_init ();
+    let state = generate_starting_state () in
+    let renderer = Renderer.make_from_state state in
+    Renderer.loop renderer state GameState.step
+  with e ->
+    Printf.eprintf "Exception: %s\n" (Printexc.to_string e);
+    (* Printexc.print_backtrace stderr; *)
+    exit 1
