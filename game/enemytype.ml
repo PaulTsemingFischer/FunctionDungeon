80097dfe1b3ea61165b31cd69fe3165ff10a27f0
mae97 <michaelaeichel@gmail.com>
2025-05-13 10:42:39 -0400

    enemytype testing and modifiers commenting

--- game/enemytype.ml
+++ game/enemytype.ml
@@ -12,7 +12,7 @@ type enemy =
   (* player can't see more than r radius around current position for t turns *)
   | Variable_Range of int (* can attack within r range *)
   | Variable_Damage of float (* attacks with d damage *)
-  | Variable_Damage_and_Range of
+  | Variable_Range_and_Damage of
       int
       * float (* extra powerful enemy, attacks with d damage within r range *)
 
@@ -24,6 +24,6 @@ let string_of_enemy (e : enemy) =
   | Thief -> "thief"
   | Blinder t -> "blinder"
   | Fog_Cloud (r, t) -> "fog cloud"
-  | Variable_Range r -> "long range enemy"
+  | Variable_Range r -> "variable range enemy"
   | Variable_Damage d -> "variable damage enemy"
-  | Variable_Damage_and_Range (r, d) -> "variable range and damage enemy"
+  | Variable_Range_and_Damage (r, d) -> "variable range and damage enemy"
