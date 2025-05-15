28ae65ad7061c6c2a388e28c16a4007bf3de7970
mae97 <michaelaeichel@gmail.com>
2025-05-15 17:31:09 -0400

    fix enemy_attacks

--- game/enemyaction.ml
+++ game/enemyaction.ml
@@ -11,16 +11,17 @@ let enemy_action (state : GameState.t) (entity : GameEntity.t)
       (GameEntity.t * Modifiers.possible_action) option =
     List.fold_left
       (fun player_opt (pos, attack) ->
-        match
-          GameWorld.query_pos
-            (GameState.room state)
-            (add_vec2 pos entity.pos)
-        with
-        | Some e -> (
-            match e.entity_type with
-            | Player -> Some (e, (add_vec2 pos entity.pos, attack))
-            | _ -> None)
-        | None -> None)
+        let absolute_pos = add_vec2 pos entity.pos in
+
+        match player_opt with
+        | Some _ -> player_opt
+        | None -> (
+            match GameWorld.query_pos (GameState.room state) absolute_pos with
+            | Some e -> (
+                match e.entity_type with
+                | Player -> Some (e, (absolute_pos, attack))
+                | _ -> None)
+            | None -> None))
       None possible_attacks
   in
 
@@ -32,6 +33,6 @@ let enemy_action (state : GameState.t) (entity : GameEntity.t)
       in
       let random_direction = random_element possible_moves in
       let target_position = add_vec2 random_direction entity.pos in
-      if GameWorld.mem_pos (GameState.room updated_state) target_position
-      then Transformations.say updated_state entity "<enemy noises>"
+      if GameWorld.mem_pos (GameState.room updated_state) target_position then
+        Transformations.say updated_state entity "<enemy noises>"
       else Transformations.apply_move updated_state entity random_direction
